14861e95acb3e3f7084a326b90090188373c52f1
Sammers21 <titantins@gmail.com>
2017-12-29 16:14:12 +0300

    add BaseTest for engine testing

---
+++ CFSM-engine/src/test/scala/cfsm/engine/TestBase.scala
@@ -0,0 +1,52 @@
+/*
+*  MIT License
+*  Copyright (c) 2017 PAIS-Lab-Public-Projects
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+*/
+
+package cfsm.engine
+
+import cfsm.domain.CFSMConfiguration
+import cfsm.parser.Parser
+import io.vertx.core.Vertx
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpec}
+
+/**
+  * A base test template for engine module
+  */
+class TestBase extends FlatSpec with BeforeAndAfterEach with BeforeAndAfterAll {
+  val vertx: Vertx = Vertx.vertx()
+  val parser = new Parser(vertx)
+
+  override protected def afterAll(): Unit = {
+    vertx.close()
+  }
+
+  /**
+    * Execute something in context of config
+    * @param pathToConfig where a config stored
+    * @param handler a function to call with config object
+    */
+  def withConfig(pathToConfig: String)(handler: CFSMConfiguration => Unit): Unit = {
+    val config = parser.parse(pathToConfig)
+    handler(config)
+  }
+}
+
